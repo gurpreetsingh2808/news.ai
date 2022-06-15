84658dea2b3d8c02482cf36dcaa1e1abec582c65
Gurpreet Singh <gurpreetsingh@gmail.com>
2022-06-16 01:04:47 +0530

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import './styles.js';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { useState, useEffect } from 'react';
 import NewsCards from './components/NewsCards/NewsCards';
+import classNames from 'classnames';
+import useStyles from './styles.js';
 
 const alanKey = 'c0baf96f186d1c212184ec8b7b1b9f1f2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
-
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
 
   useEffect(() => {
@@ -21,8 +23,10 @@ function App() {
     });
   }, []);
   return (
-    <div className="App">
-       <h1>Alan AI News Application</h1>
+    <div>
+       <div className={classes.logoContainer}>
+          <img src="https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg" className={classes.alanLogo} alt="Alan logo" />
+       </div>
        <NewsCards articles={newsArticles} />
     </div>
   );
